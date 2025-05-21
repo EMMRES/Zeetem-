e3a67c41bbb93fe0bdf1bcd060e66d568b607ea5
Adeusi emmanuel <123315114+EMMRES@users.noreply.github.com>
2025-05-22 00:21:51 +0100

    Update site before deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/Zeetem/', // Use the exact repo name here
   plugins: [react()],
 });
