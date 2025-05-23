ff8d832e8f9801adfb87e099ecf60eca9529d2dd
Adeusi emmanuel <123315114+EMMRES@users.noreply.github.com>
2025-05-23 07:47:41 +0100

    Update for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/Zeetem/', // Use the exact repo name here
+  base: '/Zeetem/',  // exact repo name with trailing slash
   plugins: [react()],
 });
